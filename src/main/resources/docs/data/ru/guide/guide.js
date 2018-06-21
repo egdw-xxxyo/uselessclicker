7cc05f475e5b6971688c3339b91799bd0b43e45b
dikobraz <dikhims@gmail.com>
2018-06-21 19:44:31 +0300

    onEvent in guide

--- src/main/resources/docs/data/ru/guide/guide.js
+++ src/main/resources/docs/data/ru/guide/guide.js
@@ -12,6 +12,7 @@ const sidemenu = {
     "delaySystem.html": ["Система задержек", "text", ""],
     "delayMultiplier.html": ["Скорость и множитель задержек", "text", ""],
     "delaysInCombined.html": ["Задержки в combined.run", "text", ""],
+    "onEvent.html": ["Вызов методов по событию", "text", ""],
     "../examples/examples.html": ["Примеры", "icon", "glyphicon glyphicon-file"]
 };
 
