6a1f72edfdb14d0309cf125613070d165deefe08
Serhii Muslanov <dikhims@gmail.com>
2018-06-25 13:55:49 +0300

    input added

---
+++ src/main/resources/docs/js/root.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    /*
+    $(".btn-expand-collapse").click(function (e) {
+        $('.navbar-primary').toggleClass('collapsed');
+
+        e.preventDefault();
+    });
+
+    $.each(sidemenu, function (page, data) {
+        let $a = $("<a>")
+            .attr("href", page);
+
+        let $icon = $("<span>");
+        let $text = $("<span>")
+            .addClass('nav-label')
+            .text(data[0]);
+
+        if (data[1] === "icon") {
+            $icon.addClass(data[2]);
+            $a.addClass('list-level-1');
+        } else {
+            $a.addClass('list-level-2');
+        }
+
+
+        if(current_page === page){
+            $a.addClass('current-page-item-menu');
+        }
+
+        $a.append($icon);
+        $a.append($text);
+        $(".navbar-primary-menu li").append($a);
+    });
+*/
+    $(".copy-button").click(function (e) {
+        $(e.target).parent().css( "background-color", "red" );
+        let text =$(e.target).parent().select("code").text();
+        alert(text);
+    });
+});
