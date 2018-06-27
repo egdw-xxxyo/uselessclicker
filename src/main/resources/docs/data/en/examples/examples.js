4b302342f5eeedff2ad694ee5a9e04e4c6671fa7
Serhii Muslanov <dikhims@gmail.com>
2018-06-27 17:06:11 +0300

    Some changes

--- src/main/resources/docs/data/en/examples/examples.js
+++ src/main/resources/docs/data/en/examples/examples.js
@@ -2,6 +2,7 @@ const sidemenu = {
     "../index.html": ["Main page", "icon", "glyphicon glyphicon-home"],
     "../guide/guide.html": ["User's guide", "icon", "glyphicon glyphicon-list"],
     "examples.html": ["Examples", "icon", "glyphicon glyphicon-file"],
+    "infinityTimes.html": ["Endless execution", "text", ""],
     "files.html": ["Files", "text", ""],
     "math.html": ["Math", "text", ""],
     "doubleClick.html": ["Double clicks", "text", ""],
