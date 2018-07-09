71a30360e97544970ab904feab966a0412bf111c
Serhii Muslanov <dikhims@gmail.com>
2018-07-09 22:17:20 +0300

    Point object

--- src/main/java/org/dikhim/jclicker/jsengine/objects/CreateObject.java
+++ src/main/java/org/dikhim/jclicker/jsengine/objects/CreateObject.java
@@ -2,6 +2,10 @@ package org.dikhim.jclicker.jsengine.objects;
 
 import org.dikhim.jclicker.jsengine.objects.Classes.Image;
 
+import java.awt.*;
+
 public interface CreateObject {
     Image image(int width, int height);
+
+    Point point(int x, int y);
 }
--- src/main/java/org/dikhim/jclicker/jsengine/objects/JsCreateObject.java
+++ src/main/java/org/dikhim/jclicker/jsengine/objects/JsCreateObject.java
@@ -2,9 +2,16 @@ package org.dikhim.jclicker.jsengine.objects;
 
 import org.dikhim.jclicker.jsengine.objects.Classes.Image;
 
+import java.awt.*;
+
 public class JsCreateObject implements CreateObject {
     @Override
     public Image image(int width, int height) {
         return new Image(width, height);
+    }
+
+    @Override
+    public Point point(int x, int y) {
+        return new Point(x, y);
     }
 }
