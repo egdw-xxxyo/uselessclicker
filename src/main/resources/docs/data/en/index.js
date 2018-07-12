1978ab60f66d9112c4f7d0f6d7548804e83cd644
Serhii Muslanov <dikhims@gmail.com>
2018-07-12 17:56:06 +0300

    Added versionHistory files

--- src/main/resources/docs/data/en/index.js
+++ src/main/resources/docs/data/en/index.js
@@ -1,5 +1,6 @@
 const sidemenu = {
     "index.html": ["Main page", "icon", "glyphicon glyphicon-home"],
+    "versionHistory.html": ["Version history", "text", ""],
     "guide/guide.html": ["User's guide", "icon", "glyphicon glyphicon-list"],
     "examples/examples.html": ["Examples", "icon", "glyphicon glyphicon-file"]
 };
