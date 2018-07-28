6d5648d76a9d550dd406e68d54248b7116151045
Serhii Muslanov <dikhims@gmail.com>
2018-07-28 09:15:02 +0300

    Clear context menu item outtextArea

--- src/main/java/org/dikhim/jclicker/ui/OutTextArea.java
+++ src/main/java/org/dikhim/jclicker/ui/OutTextArea.java
@@ -1,6 +1,11 @@
 package org.dikhim.jclicker.ui;
 
+import com.sun.javafx.scene.control.skin.TextAreaSkin;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.Clipboard;
 import javafx.scene.layout.AnchorPane;
 
 public class OutTextArea extends TextArea {
@@ -11,8 +16,22 @@ public class OutTextArea extends TextArea {
         AnchorPane.setRightAnchor(this, 0d);
         AnchorPane.setBottomAnchor(this, 0d);
         AnchorPane.setLeftAnchor(this, 0d);
-        
+
         addChangeListener(() -> appendText(""));
+
+        TextAreaSkin customContextSkin = new TextAreaSkin(this) {
+            @Override
+            public void populateContextMenu(ContextMenu contextMenu) {
+                super.populateContextMenu(contextMenu);
+
+                MenuItem insert = new MenuItem("Clear");
+                insert.setOnAction(event -> OutTextArea.this.clear());
+
+                contextMenu.getItems().add(0, new SeparatorMenuItem());
+                contextMenu.getItems().add(0, insert);
+            }
+        };
+        this.setSkin(customContextSkin);
     }
 
     public void addChangeListener(Runnable runnable) {
